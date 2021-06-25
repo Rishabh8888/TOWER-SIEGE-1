562ba1ab315bb33561c95b48812549132201c03e
Ꮢɪsʜᴀʙʜ <79573738+Rishabh8888@users.noreply.github.com>
2021-06-25 13:01:09 +0530

    Update rectangle.js

--- rectangle.js
+++ rectangle.js
@@ -1,8 +1,11 @@
 class Rectangle {
     constructor(x,y,height,width) {
       var options ={
-          isStatic:true,
-      }
+          isStatic:false,
+          restitution:0.8,
+          friction:1.0,
+          density:1.0
+       }
       this.body=Bodies.rectangle(x,y,width,height,options);
       this.height=height;
       this.width=width;
